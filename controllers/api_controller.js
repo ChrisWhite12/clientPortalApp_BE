c0b2920648000c93186bdb29daf7599bdc7a9132
ChrisWhite12 <chris_white_12@hotmail.com>
2020-12-16 11:36:32 +1000

    updating api call to return patient and appointments

--- controllers/api_controller.js
+++ controllers/api_controller.js
@@ -3,35 +3,53 @@ const fetch = require('node-fetch')
 const { Base64 } = require('js-base64')
 const { response } = require('express')
 
-const apiTemplate = (query_val,req,res) => {
-    // console.log("query:" + query_val)
-    fetch(`https://api.au2.cliniko.com/v1/${query_val}`, {
+const readPatient = (req,res) => {
+    let patient_out = {}
+    console.log(req.user.email)
+    fetch(`https://api.au2.cliniko.com/v1/patients?q=email:=${req.user.email}`, {
         headers: {
-        Accept: "application/json",
-        Authorization: `Basic ${Base64.encode(process.env.API_KEY2)}`,
-        // Authorization: `Basic TVMwMU1URTNNRFE1TXpBek1ESTFOalE1TXpBdGNUWnVlSFp0YWxKMk56Um9NVmRLYmtkS1VrOWtUSFZ0ZDNZMmQzVXpiRmMtYXUyOg==`,
-        "User-Agent": "Chris White (chris_white_12@hotmail.com)",
-        // "User-Agent": "Caity McC (ferguselchancho@gmail.com)"
+            Accept: "application/json",
+            Authorization: `Basic ${Base64.encode(process.env.API_KEY2)}`,
+            "User-Agent": "Chris White (chris_white_12@hotmail.com)",
         }
     })
     .then(response => response.json())
-    .then(data => {
-        res.status(200);
-        res.send(data)
+    .then(pat_data => {
+        // console.log(pat_data.patients[0].appointments.links.self)
+        const curr_date = new Date(Date.now()).toJSON()
+        // console.log(curr_date)
+        //---filter appointments by after current time (?q=appointment_start:>2020-01-01-T08:30:00Z)-----## UTC
+        fetch(`${pat_data.patients[0].appointments.links.self}?q=appointment_start:>${curr_date}`, {
+            headers: {
+                Accept: "application/json",
+                Authorization: `Basic ${Base64.encode(process.env.API_KEY2)}`,
+                "User-Agent": "Chris White (chris_white_12@hotmail.com)",
+            }
+        })
+        .then(app_res => app_res.json())
+        .then(app_data => {
+            // fetch(app_data.appointments[0].links.self, {
+            //     headers: {
+            //         Accept: "application/json",
+            //         Authorization: `Basic ${Base64.encode(process.env.API_KEY2)}`,
+            //         "User-Agent": "Chris White (chris_white_12@hotmail.com)",
+            //     }
+            // })
+
+            patient_out = {
+                patient: pat_data.patients[0],
+                appointments: app_data.appointments
+            }
+            res.status(200);
+            console.log(patient_out)
+            res.send(patient_out)
+        }
+        )
+        
     })
     .catch((err) => console.log(err))
 }
 
-const readPatients = (req,res) => {
-    console.log(req.user.email)
-    const patient_search = req.user.email ? `?q=email:=${req.user.email}` : '';
-    apiTemplate(`patients${patient_search}`,req,res)
-}
-const readAppointments = (req,res) => {
-    apiTemplate('appointment_types',req,res)
-}
-
 module.exports = {
-    readAppointments,
-    readPatients
+    readPatient
 }
--- routes/api_routes.js
+++ routes/api_routes.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const { readAppointments, readPatients } = require("../controllers/api_controller")
+const { readPatient } = require("../controllers/api_controller")
 
 
-router.get('/patients', readPatients)
-router.get('/appointments', readAppointments)
+router.get('/patient', readPatient)
 module.exports = router
