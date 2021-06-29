7e9ee4cbe6ab3cfc4a7f7ef0501a5a483f13a749
Chrisw <chris_white_12@hotmail.com>
2021-06-29 11:44:31 +1000

    removing twilio credietails

--- routes/phone_routes.js
+++ routes/phone_routes.js
@@ -6,8 +6,7 @@ const MessagingResponse = require('twilio').twiml.MessagingResponse
 router.post('/', (req, res) => {
     const client = require('twilio')(process.env.ACC_SID, process.env.AUTH_TOKEN)
     console.log('in phone post request - ', req.body)
-    // client('AC0f3d76c8a30cf077431cba07e51d1400', 'c3b0570ec95f0a6a6780456f85f4c704')
-    // client(process.env.ACC_SID, process.env.AUTH_TOKEN)
+
     client.messages
     .create({
         body: req.body.text,
